65b0c1f6af306459a78d530ae14b7fc65c301e28
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-01-07 13:31:56 +0330

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import Order from './components/Order'
-
-
+import React from "react";
+import Order from "./components/Order";
 
 function App() {
   return (
-   <main className=' flex flex-col justify-center items-center bg-[#E0E8FF] bg-[url(../public/images/pattern-background-mobile.svg)] bg-no-repeat bg-contain md:bg-[url(../public/images/pattern-background-desktop.svg)] min-h-screen  '>
-    <Order />
-   </main>
-  )
+    <main className=" flex flex-col justify-center items-center bg-[#E0E8FF] bg-[url(../public/images/pattern-background-mobile.svg)] bg-no-repeat bg-contain md:bg-[url(../public/images/pattern-background-desktop.svg)] min-h-screen">
+      <Order />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      red: ["Red Hat Display"],
+    },
     extend: {},
   },
   plugins: [],
